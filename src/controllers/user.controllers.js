069edc0ff998abb868ccc47989997a0d8e0300b4
Giovane <giovanec.melo@hotmail.com>
2025-08-25 01:08:14 -0300

    Add refreshAccessToken functionality and rename token generation method

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -3,9 +3,10 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.models.js";
 import { uploadOnCloudinary, deleteFromCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken";
 
 
-const generateAndRefreshToken = async(userId) => {
+const generateAccessAndRefreshToken = async(userId) => {
   try {
     const user = await User.findById(userId)
     if (!user) {
@@ -159,7 +160,61 @@ const loginUser = asyncHandler( async (req, res) => {
     ))
 })
 
+const refreshAccessToken = asyncHandler( async (req, res) => {
+  const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+
+  if (!incomingRefreshToken){
+    throw new ApiError(401, "Refresh token is required")
+  }
+
+  try {
+    jwt.verify(
+      incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    )
+    const user = await User.findById(decodedToken?._id)
+
+    if (!user){
+      throw new ApiError(401, "invalid refresh token")
+    }
+
+    if (incomingRefreshToken !== user?.refreshToken){
+      throw new ApiError(401, "Invalid refresh token")
+    }
+
+    const options = {
+      http: true,
+      secure: process.env.NODE_ENV === "production",
+    }
+
+    const {accessToken, refreshToken: newRefreshToken} = await generateAccessAndRefreshToken(user._id)
+
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", newRefreshToken, options)
+      .json(
+        new ApiResponse(
+          200,
+          {accessToken, refreshToken: newRefreshToken
+          },
+          "Access token refreshed successfully"
+        ));
+
+  } catch (error) {
+    throw new ApiError(500, "Something went wrong while refreshing access token")
+  }
+})
+
+const logoutUser = asyncHandler( async (req, res) => {
+  await User.findByIdAndUpdate(
+    //retornar apos o video de middleware
+
+  )
+})
+
 export {
   registerUser,
-  loginUser
+  loginUser,
+  refreshAccessToken
  };
